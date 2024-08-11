71817e11966706a02789bb845ec5b02d279ed2a6
Temuulen Sodnom <temuulensodnom@gmail.com>
2024-08-11 17:16:06 +0800

    no jekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+};
 
 export default nextConfig;
